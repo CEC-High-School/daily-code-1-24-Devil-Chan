a4559a75d678e6a2c2c854cb7c984f1321f82cdf
Devil-Chan <35504477+Devil-Chan@users.noreply.github.com>
2018-01-24 13:02:10 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,13 @@
-
-
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+#include <iostream>
+using namespace std;
+int main (){
+	int input;
+	cout << "input a number pls" << endl;
+	cin >> input;
+	if(input<50)
+		cout << "you suck"<<endl;
+	if (input > 100)
+		cout << "you cool" << endl;
+	else
+		cout << "meh";
+}
